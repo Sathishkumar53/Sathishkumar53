e08b0b5087e5103cedda907fd250a41009611155
sathishli53 <sathishkumr0503@gmail.com>
2022-08-18 13:17:02 +0530

    Added sathishkl 3 m

--- src/test/java/com/hexa/ClassA.java
+++ src/test/java/com/hexa/ClassA.java
@@ -1,5 +1,22 @@
 package com.hexa;
 
 public class ClassA {
+	private void method12() {
+		System.out.println("1");
 
+	}
+	private void method13() {
+		System.out.println("1");
+
+	}
+	private void method41() {
+		System.out.println("1");
+		
+
+	}
+	
+	private void method13() {
+		System.out.println("1");
+
+	}
 }
